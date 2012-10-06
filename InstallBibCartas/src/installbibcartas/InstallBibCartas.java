d6ab222cb9a20d16b26fd52459346772047feac4
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-10-05 21:04:11 -0300

    [Start Project] Adding project

---
+++ InstallBibCartas/src/installbibcartas/InstallBibCartas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package installbibcartas;
+
+/**
+ *
+ * @author Juliano R. Macedo
+ * @version 0.1
+ * @since 01/10/2012
+ */
+public class InstallBibCartas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
